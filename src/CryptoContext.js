7255c0ff1e5ceadebc8dbbc5f638d693c95de275
Rahul Padhy <rahulpadhyrkp@gmail.com>
2022-08-04 07:24:06 +0530

    firebase part started

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,20 +1,33 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { createContext } from 'react'
+import { CoinList } from './config/api'
 
 const Crypto = createContext()
 
 const CryptoContext = ({children}) => {
 const [currency, setCurrency] = useState("INR");
 const [symbol, setSymbol] = useState("₹");
+const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+
+    setCoins(data);
+    setLoading(false);
+  };
+
 useEffect(() => {
   if (currency === "INR") setSymbol("₹");
   else if (currency === "USD") setSymbol("$");
 }, [currency])
 
-
   return (
-    <Crypto.Provider value={{currency,symbol,setCurrency}}>
+    <Crypto.Provider value={{currency,symbol,setCurrency,coins,loading,fetchCoins}}>
         {children}
     </Crypto.Provider>
   )
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuptvHCqTqp5MkSwCWQ6jjXR7wOxydvaI",
+    authDomain: "crypto-hunter-96281.firebaseapp.com",
+    projectId: "crypto-hunter-96281",
+    storageBucket: "crypto-hunter-96281.appspot.com",
+    messagingSenderId: "34943752711",
+    appId: "1:34943752711:web:2b53a2f473095c194068a2"
+  };
+
+export default firebaseConfig;
